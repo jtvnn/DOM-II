28385f1f10dc5c3db3f58034b1e1b9b5bf99ed7c
Jason Vann <vannjt1@icloud.com>
2021-08-03 05:25:08 -0400

    2 - 4 tasks complete

--- js/index.js
+++ js/index.js
@@ -6,3 +6,18 @@ busImg.addEventListener('mouseover', function (event){
     busImg.style.border = 'thick solid #0000FF'
 })
 
+//2-keydown
+const mapImg = document.querySelector('.img-content img');
+
+document.addEventListener('keydown', function (event){
+    if(event.key === 'Escape'){
+        mapImg.style.border = 'thick solid #0000FF'
+    }
+})
+
+//3-wheel
+const introH2 = document.querySelector('.intro h2')
+
+introH2.addEventListener('wheel', function (event) {
+    introH2.style.transform = 'rotate(30deg)'
+})

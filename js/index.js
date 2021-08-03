2658bf1f0ac57c6524cb3601f00625a5b999bdc5
Jason Vann <vannjt1@icloud.com>
2021-08-02 20:49:52 -0400

    Mouse over complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+//1-mouseover
+const busImg = document.querySelector('.intro img')
+
+busImg.addEventListener('mouseover', function (event){
+    busImg.style.border = 'thick solid #0000FF'
+})
+
